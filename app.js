06787a99ee8f5654f98ed507ab0aa7e39f9cc457
SHresTho12 <talimul12bari@gmail.com>
2023-02-09 12:41:25 +0600

    dot env

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 
 const app = express();
 const port = 3000;
+require("dotenv").config({ path: "./.env" });
 //middleware
 
 app.use(express.json());
@@ -13,6 +14,6 @@ app.get("/", (req, res) => {
 });
 
 //server listening
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port: ${process.env.PORT}`);
 });
